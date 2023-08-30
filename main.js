b44f17780c7e0fabab397357b52a69716688d048
Kari Rehe <karinicole84@gmail.com>
2023-08-30 15:51:27 -0400

    Loop exercises completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,35 @@
+/* loop - a programming element that repeats a portion of code a set number of times until the desired process is complete. 
+iteration - a sequence of instructions or code being repeated until the specific end result is achieved. 
+increment - used to increase the value of a variable. It is represented by the ++ symbol.
+
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for(var i = 0; i <cohortOneStudents.length; i++){
+    console.log (cohortOneStudents[i]);
+}
+/*
+Sable
+David
+Joey
+Nick
+Tommy
+Connor
+Charles
+Russ
+Bobby
+Josh
+Nikki
+Matt
+Dale
+Sydney
+*/
+
+
+let string = "One of my interests is:"
+
+let karisInterests = ["homeschooling", "graphic design", "photography", "reading", "mom life"]
+
+for(let i = 0; i < karisInterests.length; i++) {
+    console.log(`One of my interests is: ${karisInterests[i]}`)
+}
+
